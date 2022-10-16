153e7ea1cdd2d180a9510fc6b71652b6499e4dd5
Bård <bard.ove.hoel@kartverket.no>
2022-10-16 23:13:00 +0200

    configmap created

--- controllers/configmap.go
+++ controllers/configmap.go
@@ -47,20 +47,21 @@ func (r *ConfigMapReconciler) Reconcile(ctx context.Context, application *skiper
 	application.FillDefaults()
 
 	gcpAuthConfigMapName := application.Name + "-gcp-auth"
+
 	gcpAuthConfigMap := corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: application.Namespace, Name: gcpAuthConfigMapName}}
-	gcpIdentityConfigMap := corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: "skiperator-system", Name: "gcpIdentity-config"}}
+	gcpIdentityConfigMap := corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Namespace: "skiperator-system", Name: "gcpidentityconfig"}}
+	
+	
 	_, err := ctrlutil.CreateOrPatch(ctx, r.client, &gcpAuthConfigMap, func() error {
-		// Set application as owner of the sidecar
+		// Set application as owner of the configmap
 		err := ctrlutil.SetControllerReference(application, &gcpAuthConfigMap, r.scheme)
 		if err != nil {
 			return err
 		}
-		
-		var ConfStruct Config
 
-		ConfStruct = Config {
+		ConfStruct := Config {
 			Type: "external_account",
-			Audience: "identitynamespace:" + gcpIdentityConfigMap.Data.workloadIdentityPool + ":" + gcpIdentityConfigMap.Data.identityProvider,
+			Audience: "identitynamespace:" + gcpIdentityConfigMap.Data["workloadIdentityPool"] + ":" + gcpIdentityConfigMap.Data["identityProvider"],
 			ServiceAccountImpersonationUrl: "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/" + application.Spec.GCP.Auth.ServiceAccount + "@" + application.Spec.GCP.Auth.Project + ".iam.gserviceaccount.com:generateAccessToken",
 			SubjectTokenType: "urn:ietf:params:oauth:token-type:jwt",
 			TokenUrl: "https://sts.googleapis.com/v1/token",
@@ -72,32 +73,26 @@ func (r *ConfigMapReconciler) Reconcile(ctx context.Context, application *skiper
 		ConfByte, err := json.Marshal(ConfStruct)
 		if err != nil {
 			return err
-		} 
-
-		gcpAuthConfigMap.Data.Config = string(ConfByte)
-
+		}
+		ConMap := map[string]string{}
+		json.Unmarshal(ConfByte, &ConMap) 
 		
-
+		
+		gcpAuthConfigMap.Data = ConMap
+		
 		return nil
 	})
+
 	return reconcile.Result{}, err
 }
 
-//kind: ConfigMap
-//apiVersion: v1
-//metadata:
-//  namespace: shipwreck
-//  name: my-cloudsdk-config
-//data:
-//  config: |
-//    {
-//      "type": "external_account",
-//      "audience": "identitynamespace:kubernetes-dev-94b9.svc.id.goog:https://gkehub.googleapis.com/projects/kubernetes-dev-94b9/locations/global/memberships/atkv1-dev",
-//      "service_account_impersonation_url": "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/secret-accessor@skip-dev-7d22.iam.gserviceaccount.com:generateAccessToken",
-//      "subject_token_type": "urn:ietf:params:oauth:token-type:jwt",
-//      "token_url": "https://sts.googleapis.com/v1/token",
-//      "credential_source": {
-//        "file": "/var/run/secrets/tokens/gcp-ksa/token"
-//      }
-//    }
-//
+//ConfStruct := Config {
+//	Type: "external_account",
+//	Audience: "identitynamespace:" + string(gcpIdentityConfigMap.Data.workloadIdentityPool) + ":" + string(gcpIdentityConfigMap.Data.identityProvider),
+//	ServiceAccountImpersonationUrl: "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/" + application.Spec.GCP.Auth.ServiceAccount + "@" + application.Spec.GCP.Auth.Project + ".iam.gserviceaccount.com:generateAccessToken",
+//	SubjectTokenType: "urn:ietf:params:oauth:token-type:jwt",
+//	TokenUrl: "https://sts.googleapis.com/v1/token",
+//	CredentialSource: CredentialSource{
+//		File: "/var/run/secrets/tokens/gcp-ksa/token",
+//	} ,
+//}
