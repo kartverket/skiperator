7992bf76f9258de86402e4c0f112f665f34e8bbf
anderssonw <wandersson.98@gmail.com>
2023-01-04 13:19:49 +0100

    add todo comment

--- api/v1alpha1/application.go
+++ api/v1alpha1/application.go
@@ -71,6 +71,9 @@ type ApplicationSpec struct {
 
 // +kubebuilder:object:generate=true
 type ResourceRequirements struct {
+	// TODO
+	// Remember to reasess whether or not Claims work properly with kubebuilder when we upgrade to Kubernetes 1.26
+
 	//+kubebuilder:validation:Optional
 	Limits corev1.ResourceList `json:"limits,omitempty"`
 
