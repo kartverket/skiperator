696f081861a0252cd6b824a6e4b1ce3c8647fd9c
anderssonw <wandersson.98@gmail.com>
2023-02-07 10:29:22 +0100

    add namespace exclusions with configmap

--- controllers/namespace/controller.go
+++ controllers/namespace/controller.go
@@ -2,12 +2,16 @@ package namespacecontroller
 
 import (
 	"context"
+	"fmt"
 
 	util "github.com/kartverket/skiperator/pkg/util"
+	"golang.org/x/exp/slices"
 	istionetworkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -19,15 +23,76 @@ type NamespaceReconciler struct {
 	Registry string
 }
 
+var excludedNamespaceList = []string{
+	// System namespaces
+	"istio-system",
+	"kube-node-lease",
+	"kube-public",
+	"kube-system",
+	"skiperator-system",
+	"config-management-system",
+	"config-management-monitoring",
+	"asm-system",
+	"anthos-identity-service",
+	"binauthz-system",
+	"cert-manager",
+	"gatekeeper-system",
+	"gke-connect",
+	"gke-system",
+	"gke-managed-metrics-server",
+	"resource-group-system",
+	// Bundles NetworkPolicies already
+	"kasten-io",
+	// TODO needs NetworkPolicies/Skiperator
+	"vault",
+	// TODO PoC, add NetworkPolicies after
+	"sysdig-agent",
+	"sysdig-admission-controller",
+	"instana-agent",
+	"kubecost",
+	"argocd",
+	"crossplane-system",
+	"upbound-system",
+	"instana-autotrace-webhook",
+	"fluentd", //POC
+}
+
+func (r *NamespaceReconciler) isExcludedNamespace(ctx context.Context, namespace string) bool {
+	configMapNamespacedName := types.NamespacedName{Namespace: "skiperator-system", Name: "namespace-exclusions"}
+	namespaceExclusionCMap := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      configMapNamespacedName.Name,
+			Namespace: configMapNamespacedName.Namespace,
+		},
+	}
+
+	err := r.GetClient().Get(context.Background(), configMapNamespacedName, &namespaceExclusionCMap)
+	if errors.IsNotFound(err) {
+		err = r.GetClient().Create(ctx, &namespaceExclusionCMap)
+		if err != nil {
+			fmt.Printf("Something went wrong creating namespace-exclusion config map: %v", err.Error())
+			return true
+		}
+	}
+	if err != nil {
+		fmt.Printf("Something went wrong getting namespace-exclusion config map: %v", err.Error())
+		return true
+	}
+
+	nameSpacesToExclude := namespaceExclusionCMap.Data
+
+	exclusion, keyExists := nameSpacesToExclude[namespace]
+
+	return (keyExists && exclusion == "exclude") || slices.Contains(excludedNamespaceList, namespace)
+}
+
 //+kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch
 //+kubebuilder:rbac:groups=core,resources=secrets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=networking.istio.io,resources=sidecars,verbs=get;list;watch;create;update;patch;delete
 
 func (r *NamespaceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&corev1.Namespace{}, builder.WithPredicates(
-			util.MatchesPredicate[*corev1.Namespace](util.IsNotExcludedNamespace),
-		)).
+		For(&corev1.Namespace{}).
 		Owns(&networkingv1.NetworkPolicy{}).
 		Owns(&istionetworkingv1beta1.Sidecar{}).
 		Owns(&corev1.Secret{}, builder.WithPredicates(
@@ -39,7 +104,6 @@ func (r *NamespaceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 func (r *NamespaceReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	namespace := &corev1.Namespace{}
 	err := r.GetClient().Get(ctx, req.NamespacedName, namespace)
-
 	if errors.IsNotFound(err) {
 		return reconcile.Result{}, nil
 	} else if err != nil {
@@ -48,6 +112,11 @@ func (r *NamespaceReconciler) Reconcile(ctx context.Context, req reconcile.Reque
 			corev1.EventTypeNormal, "ReconcileStartFail",
 			"Something went wrong fetching the namespace. It might have been deleted",
 		)
+		return reconcile.Result{}, err
+	}
+
+	if r.isExcludedNamespace(ctx, namespace.Name) {
+		fmt.Printf("Namespace %s is excluded, skipping further reconciliation\n", namespace.Name)
 		return reconcile.Result{}, err
 	}
 
--- pkg/util/helperfunctions.go
+++ pkg/util/helperfunctions.go
@@ -3,53 +3,12 @@ package util
 import (
 	"hash/fnv"
 	"regexp"
-
-	"golang.org/x/exp/slices"
-	corev1 "k8s.io/api/core/v1"
 )
 
 var internalPattern = regexp.MustCompile(`[^.]\.skip\.statkart\.no`)
 
 func IsInternal(hostname string) bool {
 	return internalPattern.MatchString(hostname)
-}
-
-var excludedNamespaces = []string{
-	// System namespaces
-	"istio-system",
-	"kube-node-lease",
-	"kube-public",
-	"kube-system",
-	"skiperator-system",
-	"config-management-system",
-	"config-management-monitoring",
-	"asm-system",
-	"anthos-identity-service",
-	"binauthz-system",
-	"cert-manager",
-	"gatekeeper-system",
-	"gke-connect",
-	"gke-system",
-	"gke-managed-metrics-server",
-	"resource-group-system",
-	// Bundles NetworkPolicies already
-	"kasten-io",
-	// TODO needs NetworkPolicies/Skiperator
-	"vault",
-	// TODO PoC, add NetworkPolicies after
-	"sysdig-agent",
-	"sysdig-admission-controller",
-	"instana-agent",
-	"kubecost",
-	"argocd",
-	"crossplane-system",
-	"upbound-system",
-	"instana-autotrace-webhook",
-	"fluentd", //POC
-}
-
-func IsNotExcludedNamespace(namespace *corev1.Namespace) bool {
-	return !slices.Contains(excludedNamespaces, namespace.Name)
 }
 
 func GenerateHashFromName(name string) uint64 {
