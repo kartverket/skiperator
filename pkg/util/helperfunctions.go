0f1567cc3aae87908cd61b3abeeec2899db1b2e5
anderssonw <wandersson.98@gmail.com>
2023-02-20 16:02:13 +0100

    remove util func for namespace exclusions

--- pkg/util/helperfunctions.go
+++ pkg/util/helperfunctions.go
@@ -7,7 +7,6 @@ import (
 	"regexp"
 
 	"golang.org/x/exp/maps"
-	"golang.org/x/exp/slices"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,45 +19,6 @@ var internalPattern = regexp.MustCompile(`[^.]\.skip\.statkart\.no`)
 
 func IsInternal(hostname string) bool {
 	return internalPattern.MatchString(hostname)
-}
-
-var excludedNamespaces = []string{
-	// System namespaces
-	"istio-system",
-	"kube-node-lease",
-	"kube-public",
-	"kube-system",
-	"skiperator-system",
-	"config-management-system",
-	"config-management-monitoring",
-	"asm-system",
-	"anthos-identity-service",
-	"binauthz-system",
-	"cert-manager",
-	"gatekeeper-system",
-	"gke-connect",
-	"gke-system",
-	"gke-managed-metrics-server",
-	"resource-group-system",
-	// Bundles NetworkPolicies already
-	"kasten-io",
-	// TODO needs NetworkPolicies/Skiperator
-	"vault",
-	// TODO PoC, add NetworkPolicies after
-	"sysdig-agent",
-	"sysdig-admission-controller",
-	"instana-agent",
-	"kubecost",
-	"argocd",
-	"crossplane-system",
-	"upbound-system",
-	"instana-autotrace-webhook",
-	"fluentd",          //POC
-	"external-secrets", //POC
-}
-
-func IsNotExcludedNamespace(namespace *corev1.Namespace) bool {
-	return !slices.Contains(excludedNamespaces, namespace.Name)
 }
 
 func GenerateHashFromName(name string) uint64 {
