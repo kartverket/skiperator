90e15b2da2b3447931d477b5a24a88509bd38b85
anderssonw <wandersson.98@gmail.com>
2023-04-20 15:56:05 +0200

    move labels
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- controllers/application/deployment.go
+++ controllers/application/deployment.go
@@ -37,20 +37,6 @@ func (r *ApplicationReconciler) reconcileDeployment(ctx context.Context, applica
 		r.SetLabelsFromApplication(ctx, &deployment, *application)
 		util.SetCommonAnnotations(&deployment)
 
-		labels := map[string]string{"app": application.Name}
-		deployment.Spec.Template.ObjectMeta.Labels = labels
-
-		deployment.Spec.Template.ObjectMeta.Annotations = map[string]string{
-			"argocd.argoproj.io/sync-options": "Prune=false",
-			"prometheus.io/scrape":            "true",
-		}
-
-		// add an external link to argocd
-		ingresses := application.Spec.Ingresses
-		if len(ingresses) > 0 {
-			deployment.ObjectMeta.Annotations[AnnotationKeyLinkPrefix] = fmt.Sprintf("https://%s", ingresses[0])
-		}
-
 		skiperatorContainer := corev1.Container{
 			Name:            application.Name,
 			Image:           application.Spec.Image,
@@ -72,124 +58,21 @@ func (r *ApplicationReconciler) reconcileDeployment(ctx context.Context, applica
 				Limits:   application.Spec.Resources.Limits,
 				Requests: application.Spec.Resources.Requests,
 			},
+			Env:            application.Spec.Env,
+			ReadinessProbe: getProbe(application.Spec.Readiness),
+			LivenessProbe:  getProbe(application.Spec.Liveness),
+			StartupProbe:   getProbe(application.Spec.Startup),
 		}
 
-		numberOfVolumes := len(application.Spec.FilesFrom) + 1
-		if application.Spec.GCP != nil {
-			numberOfVolumes = numberOfVolumes + 1
+		podVolumes, containerVolumeMounts := getContainerVolumeMountsAndPodVolumes(application)
+		podVolumes, containerVolumeMounts, err = r.appendGCPVolumeMount(application, ctx, &skiperatorContainer, containerVolumeMounts, podVolumes)
+		if err != nil {
+			r.SetControllerError(ctx, application, controllerName, err)
+			return err
 		}
+		skiperatorContainer.VolumeMounts = containerVolumeMounts
 
-		deployment.Spec.Template.Spec.Volumes = make([]corev1.Volume, numberOfVolumes)
-		skiperatorContainer.VolumeMounts = make([]corev1.VolumeMount, numberOfVolumes)
-		volumes := deployment.Spec.Template.Spec.Volumes
-		volumeMounts := skiperatorContainer.VolumeMounts
-
-		volumes[0] = corev1.Volume{Name: "tmp", VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}}}
-		volumeMounts[0] = corev1.VolumeMount{Name: "tmp", MountPath: "/tmp"}
-
-		for i, file := range application.Spec.FilesFrom {
-			if len(file.ConfigMap) > 0 {
-				volumes[i+1].Name = file.ConfigMap
-				volumes[i+1].ConfigMap = &corev1.ConfigMapVolumeSource{}
-				volumes[i+1].ConfigMap.Name = file.ConfigMap
-			} else if len(file.Secret) > 0 {
-				volumes[i+1].Name = file.Secret
-				volumes[i+1].Secret = &corev1.SecretVolumeSource{}
-				volumes[i+1].Secret.SecretName = file.Secret
-			} else if len(file.EmptyDir) > 0 {
-				volumes[i+1].Name = file.EmptyDir
-				volumes[i+1].EmptyDir = &corev1.EmptyDirVolumeSource{}
-			} else if len(file.PersistentVolumeClaim) > 0 {
-				volumes[i+1].Name = file.PersistentVolumeClaim
-				volumes[i+1].PersistentVolumeClaim = &corev1.PersistentVolumeClaimVolumeSource{}
-				volumes[i+1].PersistentVolumeClaim.ClaimName = file.PersistentVolumeClaim
-			}
-
-			volumeMounts[i+1] = corev1.VolumeMount{Name: volumes[i+1].Name, MountPath: file.MountPath}
-		}
-
-		if application.Spec.GCP != nil {
-			gcpIdentityConfigMapNamespacedName := types.NamespacedName{Namespace: "skiperator-system", Name: "gcp-identity-config"}
-			gcpIdentityConfigMap := corev1.ConfigMap{}
-
-			gcpIdentityConfigMap, err := util.GetConfigMap(r.GetClient(), ctx, gcpIdentityConfigMapNamespacedName)
-
-			if !util.ErrIsMissingOrNil(
-				r.GetRecorder(),
-				err,
-				"Cannot find configmap named "+gcpIdentityConfigMapNamespacedName.Name+" in namespace "+gcpIdentityConfigMapNamespacedName.Namespace,
-				application,
-			) {
-				r.SetControllerError(ctx, application, controllerName, err)
-				return err
-			}
-
-			envVar := corev1.EnvVar{
-				Name:  "GOOGLE_APPLICATION_CREDENTIALS",
-				Value: "/var/run/secrets/tokens/gcp-ksa/google-application-credentials.json",
-			}
-			skiperatorContainer.Env = append(application.Spec.Env, envVar)
-
-			volumeMounts[numberOfVolumes-1].Name = "gcp-ksa"
-			volumeMounts[numberOfVolumes-1].MountPath = "/var/run/secrets/tokens/gcp-ksa"
-			volumeMounts[numberOfVolumes-1].ReadOnly = true
-
-			twoDaysSec := int64(172800)
-			optionalBool := false
-			defaultModeValue := int32(420)
-
-			volumes[numberOfVolumes-1].Name = "gcp-ksa"
-			volumes[numberOfVolumes-1].Projected = &corev1.ProjectedVolumeSource{}
-			volumes[numberOfVolumes-1].Projected.DefaultMode = &defaultModeValue
-			volumes[numberOfVolumes-1].Projected.Sources = make([]corev1.VolumeProjection, 2)
-			volumes[numberOfVolumes-1].Projected.Sources[0].ServiceAccountToken = &corev1.ServiceAccountTokenProjection{
-				Path:              "token",
-				Audience:          gcpIdentityConfigMap.Data["workloadIdentityPool"],
-				ExpirationSeconds: &twoDaysSec,
-			}
-			volumes[numberOfVolumes-1].Projected.Sources[1].ConfigMap = &corev1.ConfigMapProjection{
-				LocalObjectReference: corev1.LocalObjectReference{
-					Name: application.Name + "-gcp-auth",
-				},
-				Optional: &optionalBool,
-				Items:    make([]corev1.KeyToPath, 1),
-			}
-			volumes[numberOfVolumes-1].Projected.Sources[1].ConfigMap.Items[0].Key = "config"
-			volumes[numberOfVolumes-1].Projected.Sources[1].ConfigMap.Items[0].Path = "google-application-credentials.json"
-		} else {
-			skiperatorContainer.Env = application.Spec.Env
-		}
-
-		if application.Spec.Readiness != nil {
-			skiperatorContainer.ReadinessProbe = &corev1.Probe{}
-			skiperatorContainer.ReadinessProbe.InitialDelaySeconds = int32(application.Spec.Readiness.InitialDelay)
-			skiperatorContainer.ReadinessProbe.TimeoutSeconds = int32(application.Spec.Readiness.Timeout)
-			skiperatorContainer.ReadinessProbe.FailureThreshold = int32(application.Spec.Readiness.FailureThreshold)
-
-			skiperatorContainer.ReadinessProbe.HTTPGet = &corev1.HTTPGetAction{}
-			skiperatorContainer.ReadinessProbe.HTTPGet.Port = intstr.FromInt(int(application.Spec.Readiness.Port))
-			skiperatorContainer.ReadinessProbe.HTTPGet.Path = application.Spec.Readiness.Path
-		}
-		if application.Spec.Liveness != nil {
-			skiperatorContainer.LivenessProbe = &corev1.Probe{}
-			skiperatorContainer.LivenessProbe.InitialDelaySeconds = int32(application.Spec.Liveness.InitialDelay)
-			skiperatorContainer.LivenessProbe.TimeoutSeconds = int32(application.Spec.Liveness.Timeout)
-			skiperatorContainer.LivenessProbe.FailureThreshold = int32(application.Spec.Liveness.FailureThreshold)
-
-			skiperatorContainer.LivenessProbe.HTTPGet = &corev1.HTTPGetAction{}
-			skiperatorContainer.LivenessProbe.HTTPGet.Port = intstr.FromInt(int(application.Spec.Liveness.Port))
-			skiperatorContainer.LivenessProbe.HTTPGet.Path = application.Spec.Liveness.Path
-		}
-		if application.Spec.Startup != nil {
-			skiperatorContainer.StartupProbe = &corev1.Probe{}
-			skiperatorContainer.StartupProbe.InitialDelaySeconds = int32(application.Spec.Startup.InitialDelay)
-			skiperatorContainer.StartupProbe.TimeoutSeconds = int32(application.Spec.Startup.Timeout)
-			skiperatorContainer.StartupProbe.FailureThreshold = int32(application.Spec.Startup.FailureThreshold)
-
-			skiperatorContainer.StartupProbe.HTTPGet = &corev1.HTTPGetAction{}
-			skiperatorContainer.StartupProbe.HTTPGet.Port = intstr.FromInt(int(application.Spec.Startup.Port))
-			skiperatorContainer.StartupProbe.HTTPGet.Path = application.Spec.Startup.Path
-		}
+		labels := map[string]string{"app": application.Name}
 
 		deployment.Spec = appsv1.DeploymentSpec{
 			Selector: &metav1.LabelSelector{MatchLabels: labels},
@@ -199,6 +82,13 @@ func (r *ApplicationReconciler) reconcileDeployment(ctx context.Context, applica
 				RollingUpdate: getRollingUpdateStrategy(application.Spec.Strategy.Type),
 			},
 			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+					Annotations: map[string]string{
+						"argocd.argoproj.io/sync-options": "Prune=false",
+						"prometheus.io/scrape":            "true",
+					},
+				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						skiperatorContainer,
@@ -211,8 +101,15 @@ func (r *ApplicationReconciler) reconcileDeployment(ctx context.Context, applica
 						FSGroup:            &util.SkiperatorUser,
 					},
 					ServiceAccountName: application.Name,
+					Volumes:            podVolumes,
 				},
 			},
+		}
+
+		// add an external link to argocd
+		ingresses := application.Spec.Ingresses
+		if len(ingresses) > 0 {
+			deployment.ObjectMeta.Annotations[AnnotationKeyLinkPrefix] = fmt.Sprintf("https://%s", ingresses[0])
 		}
 
 		return nil
@@ -223,26 +120,184 @@ func (r *ApplicationReconciler) reconcileDeployment(ctx context.Context, applica
 	return reconcile.Result{}, err
 }
 
+func getProbe(appProbe *skiperatorv1alpha1.Probe) *corev1.Probe {
+	if appProbe != nil {
+		probe := corev1.Probe{
+			InitialDelaySeconds: int32(appProbe.InitialDelay),
+			TimeoutSeconds:      int32(appProbe.Timeout),
+			FailureThreshold:    int32(appProbe.FailureThreshold),
+			ProbeHandler: corev1.ProbeHandler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path: appProbe.Path,
+					Port: intstr.FromInt(int(appProbe.Port)),
+				},
+			},
+		}
+
+		return &probe
+	}
+
+	return nil
+}
+
+func (r ApplicationReconciler) appendGCPVolumeMount(application *skiperatorv1alpha1.Application, ctx context.Context, skiperatorContainer *corev1.Container, volumeMounts []corev1.VolumeMount, volumes []corev1.Volume) ([]corev1.Volume, []corev1.VolumeMount, error) {
+	if application.Spec.GCP != nil {
+		gcpIdentityConfigMapNamespacedName := types.NamespacedName{Namespace: "skiperator-system", Name: "gcp-identity-config"}
+		gcpIdentityConfigMap := corev1.ConfigMap{}
+
+		gcpIdentityConfigMap, err := util.GetConfigMap(r.GetClient(), ctx, gcpIdentityConfigMapNamespacedName)
+
+		if !util.ErrIsMissingOrNil(
+			r.GetRecorder(),
+			err,
+			"Cannot find configmap named "+gcpIdentityConfigMapNamespacedName.Name+" in namespace "+gcpIdentityConfigMapNamespacedName.Namespace,
+			application,
+		) {
+			r.SetControllerError(ctx, application, controllerName, err)
+			return volumes, volumeMounts, err
+		}
+
+		envVar := corev1.EnvVar{
+			Name:  "GOOGLE_APPLICATION_CREDENTIALS",
+			Value: "/var/run/secrets/tokens/gcp-ksa/google-application-credentials.json",
+		}
+		skiperatorContainer.Env = append(application.Spec.Env, envVar)
+
+		volumeMounts = append(volumeMounts, corev1.VolumeMount{
+			Name:      "gcp-ksa",
+			MountPath: "/var/run/secrets/tokens/gcp-ksa",
+			ReadOnly:  true,
+		})
+
+		twoDaysInSeconds := int64(172800)
+
+		volumes = append(volumes, corev1.Volume{
+			Name: "gcp-ksa",
+			VolumeSource: corev1.VolumeSource{
+				Projected: &corev1.ProjectedVolumeSource{
+					DefaultMode: util.PointTo(int32(420)),
+					Sources: []corev1.VolumeProjection{
+						{
+							ServiceAccountToken: &corev1.ServiceAccountTokenProjection{
+								Path:              "token",
+								Audience:          gcpIdentityConfigMap.Data["workloadIdentityPool"],
+								ExpirationSeconds: &twoDaysInSeconds,
+							},
+						},
+						{
+							ConfigMap: &corev1.ConfigMapProjection{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: application.Name + "-gcp-auth",
+								},
+								Optional: util.PointTo(false),
+								Items: []corev1.KeyToPath{
+									{
+										Key:  "config",
+										Path: "google-application-credentials.json",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		})
+	}
+
+	return volumes, volumeMounts, nil
+}
+
+func getContainerVolumeMountsAndPodVolumes(application *skiperatorv1alpha1.Application) ([]corev1.Volume, []corev1.VolumeMount) {
+	containerVolumeMounts := []corev1.VolumeMount{
+		{
+			Name:      "tmp",
+			MountPath: "/tmp",
+		},
+	}
+
+	podVolumes := []corev1.Volume{
+		{
+			Name: "tmp",
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{},
+			},
+		},
+	}
+
+	for _, file := range application.Spec.FilesFrom {
+		volume := corev1.Volume{}
+		if len(file.ConfigMap) > 0 {
+			volume = corev1.Volume{
+				Name: file.ConfigMap,
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: file.ConfigMap,
+						},
+					},
+				},
+			}
+		} else if len(file.Secret) > 0 {
+			volume = corev1.Volume{
+				Name: file.Secret,
+				VolumeSource: corev1.VolumeSource{
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: file.Secret,
+					},
+				},
+			}
+		} else if len(file.EmptyDir) > 0 {
+			volume = corev1.Volume{
+				Name: file.EmptyDir,
+				VolumeSource: corev1.VolumeSource{
+					EmptyDir: &corev1.EmptyDirVolumeSource{},
+				},
+			}
+		} else if len(file.PersistentVolumeClaim) > 0 {
+			volume = corev1.Volume{
+				Name: file.PersistentVolumeClaim,
+				VolumeSource: corev1.VolumeSource{
+					PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+						ClaimName: file.PersistentVolumeClaim,
+					},
+				},
+			}
+		}
+
+		podVolumes = append(podVolumes, volume)
+		containerVolumeMounts = append(containerVolumeMounts, corev1.VolumeMount{
+			Name:      volume.Name,
+			MountPath: file.MountPath,
+		})
+	}
+
+	return podVolumes, containerVolumeMounts
+}
+
 func getEnvFrom(envFromApplication []skiperatorv1alpha1.EnvFrom) []corev1.EnvFromSource {
 	envFromSource := []corev1.EnvFromSource{}
 
-	for i, env := range envFromApplication {
+	for _, env := range envFromApplication {
 		if len(env.ConfigMap) > 0 {
-			envFromSource[i] = corev1.EnvFromSource{
-				ConfigMapRef: &corev1.ConfigMapEnvSource{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: env.ConfigMap,
+			envFromSource = append(envFromSource,
+				corev1.EnvFromSource{
+					ConfigMapRef: &corev1.ConfigMapEnvSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: env.ConfigMap,
+						},
 					},
 				},
-			}
+			)
 		} else if len(env.Secret) > 0 {
-			envFromSource[i] = corev1.EnvFromSource{
-				SecretRef: &corev1.SecretEnvSource{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: env.Secret,
+			envFromSource = append(envFromSource,
+				corev1.EnvFromSource{
+					SecretRef: &corev1.SecretEnvSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: env.Secret,
+						},
 					},
 				},
-			}
+			)
 		}
 	}
 
@@ -252,7 +307,7 @@ func getEnvFrom(envFromApplication []skiperatorv1alpha1.EnvFrom) []corev1.EnvFro
 func getContainerPorts(application *skiperatorv1alpha1.Application) []corev1.ContainerPort {
 
 	containerPorts := []corev1.ContainerPort{
-		corev1.ContainerPort{
+		{
 			Name:          "main",
 			ContainerPort: int32(application.Spec.Port),
 		},
