82745af25485290221ee4e533021e2296528a503
Even Holthe <even.holthe@kartverket.no>
2023-06-23 11:44:57 +0200

    Add a Prometheus section to the CRD

--- controllers/application/service_monitor.go
+++ controllers/application/service_monitor.go
@@ -10,8 +10,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-const MetricsPortName = "metrics"
-
 func (r *ApplicationReconciler) reconcileServiceMonitor(ctx context.Context, application *skiperatorv1alpha1.Application) (reconcile.Result, error) {
 	controllerName := "ServiceMonitor"
 	r.SetControllerProgressing(ctx, application, controllerName)
@@ -21,12 +19,16 @@ func (r *ApplicationReconciler) reconcileServiceMonitor(ctx context.Context, app
 		return reconcile.Result{}, nil
 	}
 
-	if !hasMetricsPort(application) {
+	if application.Spec.Prometheus == nil {
 		r.SetControllerFinishedOutcome(ctx, application, controllerName, nil)
 		return reconcile.Result{}, nil
 	}
 
-	serviceMonitor := pov1.ServiceMonitor{ObjectMeta: metav1.ObjectMeta{Namespace: application.Namespace, Name: application.Name}}
+	serviceMonitor := pov1.ServiceMonitor{ObjectMeta: metav1.ObjectMeta{
+		Namespace: application.Namespace,
+		Name:      application.Name,
+		Labels:    map[string]string{"instance": "primary"},
+	}}
 	_, err := ctrlutil.CreateOrPatch(ctx, r.GetClient(), &serviceMonitor, func() error {
 		// Set application as owner of the service
 		err := ctrlutil.SetControllerReference(application, &serviceMonitor, r.GetScheme())
@@ -45,9 +47,10 @@ func (r *ApplicationReconciler) reconcileServiceMonitor(ctx context.Context, app
 			NamespaceSelector: pov1.NamespaceSelector{
 				MatchNames: []string{application.Namespace},
 			},
-			Endpoints: []pov1.Endpoint{
-				{Port: "metrics"},
-			},
+			Endpoints: []pov1.Endpoint{{
+				Path:       application.Spec.Prometheus.Path,
+				TargetPort: &application.Spec.Prometheus.Port,
+			}},
 		}
 
 		return nil
@@ -56,14 +59,4 @@ func (r *ApplicationReconciler) reconcileServiceMonitor(ctx context.Context, app
 	r.SetControllerFinishedOutcome(ctx, application, controllerName, err)
 
 	return reconcile.Result{}, err
-}
-
-func hasMetricsPort(application *skiperatorv1alpha1.Application) bool {
-	for _, p := range application.Spec.AdditionalPorts {
-		if p.Name == MetricsPortName {
-			return true
-		}
-	}
-
-	return false
 }
