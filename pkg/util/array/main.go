143f11de9a383ef151819ee7c362d5fdbfe4d6ca
anderssonw <wandersson.98@gmail.com>
2023-08-04 12:21:34 +0200

    remove unused map function

--- pkg/util/array/main.go
+++ pkg/util/array/main.go
@@ -1,13 +1,5 @@
 package array
 
-func Map[F any, T any](arr []F, transformFunc func(F) T) []T {
-	res := make([]T, len(arr))
-	for i, el := range arr {
-		res[i] = transformFunc(el)
-	}
-	return res
-}
-
 func MapErr[F any, T any](arr []F, transformFunc func(F) (T, error)) ([]T, error) {
 	var err error
 
