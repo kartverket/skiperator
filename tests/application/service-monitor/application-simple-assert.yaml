kartverket/skiperator

commit d745d6aab6678a70821eff98a4708d3b9d6d3ffe

Drop some istio metrics by default in monitor objects (#436)

config/crd/skiperator.kartverket.no_skipjobs.yaml
@@ -721,6 +721,12 @@ spec:
                   Prometheus settings for pod running in job. Fields are identical to Application and if set,
                   a monitorngs object is created.
                 properties:
+                  allowAllMetrics:
+                    default: false
+                    description: |-
+                      Setting AllowAllMetrics to true will ensure all exposed metrics are scraped. Otherwise, a list of predefined
+                      metrics will be dropped by default. See util/constants.go for the default list.
+                    type: boolean
                   path:
                     default: /metrics
                     description: The HTTP path where Prometheus compatible metrics

controllers/application/service_monitor.go
@@ -9,6 +9,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"strings"
 )
 
 func (r *ApplicationReconciler) reconcileServiceMonitor(ctx context.Context, application *skiperatorv1alpha1.Application) (reconcile.Result, error) {
@@ -67,6 +68,16 @@ func (r *ApplicationReconciler) reconcileServiceMonitor(ctx context.Context, app
 					TargetPort: &util.IstioMetricsPortName,
 				},
 			},
+		}
+
+		if !application.Spec.Prometheus.AllowAllMetrics {
+			serviceMonitor.Spec.Endpoints[0].MetricRelabelConfigs = []*pov1.RelabelConfig{
+				{
+					Action:       "drop",
+					Regex:        strings.Join(util.DefaultMetricDropList, "|"),
+					SourceLabels: []pov1.LabelName{"__name__"},
+				},
+			}
 		}
 
 		return nil

tests/application/service-monitor/chainsaw-test.yaml
@@ -13,3 +13,8 @@ spec:
             file: application-istio.yaml
         - assert:
             file: application-istio-assert.yaml
+    - try:
+        - patch:
+            file: patch-application-allowall.yaml
+        - assert:
+            file: patch-application-allowall-assert.yaml

tests/skipjob/podmonitor/chainsaw-test.yaml
@@ -13,6 +13,11 @@ spec:
             file: skipjob-with-monitoring.yaml
         - assert:
             file: skipjob-with-monitoring-assert.yaml
+    - try:
+        - patch:
+            file: patch-skipjob-with-monitoring.yaml
+        - assert:
+            file: patch-skipjob-with-monitoring-assert.yaml
         - delete:
             ref:
               apiVersion: skiperator.kartverket.no/v1alpha1

api/v1alpha1/application_types.go
@@ -321,6 +321,13 @@ type PrometheusConfig struct {
 	//+kubebuilder:default:=/metrics
 	//+kubebuilder:validation:Optional
 	Path string `json:"path,omitempty"`
+
+	// Setting AllowAllMetrics to true will ensure all exposed metrics are scraped. Otherwise, a list of predefined
+	// metrics will be dropped by default. See util/constants.go for the default list.
+	//
+	//+kubebuilder:default:=false
+	//+kubebuilder:validation:Optional
+	AllowAllMetrics bool `json:"allowAllMetrics,omitempty"`
 }
 
 // ApplicationStatus

api/v1alpha1/skipjob_types.go
@@ -65,7 +65,7 @@ type SKIPJobSpec struct {
 	Container ContainerSettings `json:"container"`
 
 	// Prometheus settings for pod running in job. Fields are identical to Application and if set,
-	// a monitorngs object is created.
+	// a podmonitoring object is created.
 	Prometheus *PrometheusConfig `json:"prometheus,omitempty"`
 }
 

config/crd/skiperator.kartverket.no_applications.yaml
@@ -773,6 +773,12 @@ spec:
                 description: Optional settings for how Prometheus compatible metrics
                   should be scraped.
                 properties:
+                  allowAllMetrics:
+                    default: false
+                    description: |-
+                      Setting AllowAllMetrics to true will ensure all exposed metrics are scraped. Otherwise, a list of predefined
+                      metrics will be dropped by default. See util/constants.go for the default list.
+                    type: boolean
                   path:
                     default: /metrics
                     description: The HTTP path where Prometheus compatible metrics

controllers/skipjob/pod_monitor.go
@@ -9,6 +9,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"strings"
 )
 
 func (r *SKIPJobReconciler) reconcilePodMonitor(ctx context.Context, skipJob *skiperatorv1alpha1.SKIPJob) (reconcile.Result, error) {
@@ -49,6 +50,15 @@ func (r *SKIPJobReconciler) reconcilePodMonitor(ctx context.Context, skipJob *sk
 					TargetPort: &util.IstioMetricsPortName,
 				},
 			},
+		}
+		if !skipJob.Spec.Prometheus.AllowAllMetrics {
+			podMonitor.Spec.PodMetricsEndpoints[0].MetricRelabelConfigs = []*pov1.RelabelConfig{
+				{
+					Action:       "drop",
+					Regex:        strings.Join(util.DefaultMetricDropList, "|"),
+					SourceLabels: []pov1.LabelName{"__name__"},
+				},
+			}
 		}
 		return nil
 	})

commit 2cd41c5a1607266f796a8d16c274808404ec5e75

Fix runtime panic introduced by #436 (#437)
* Reverse logic because field in spec could be nil

* Add test for nil scenario

* Add test for podMonitor

config/crd/skiperator.kartverket.no_skipjobs.yaml
@@ -719,7 +719,7 @@ spec:
               prometheus:
                 description: |-
                   Prometheus settings for pod running in job. Fields are identical to Application and if set,
-                  a monitorngs object is created.
+                  a podmonitoring object is created.
                 properties:
                   allowAllMetrics:
                     default: false

controllers/application/service_monitor.go
@@ -66,18 +66,20 @@ func (r *ApplicationReconciler) reconcileServiceMonitor(ctx context.Context, app
 				{
 					Path:       util.IstioMetricsPath,
 					TargetPort: &util.IstioMetricsPortName,
+					MetricRelabelConfigs: []*pov1.RelabelConfig{
+						{
+							Action:       "drop",
+							Regex:        strings.Join(util.DefaultMetricDropList, "|"),
+							SourceLabels: []pov1.LabelName{"__name__"},
+						},
+					},
 				},
 			},
 		}
 
-		if !application.Spec.Prometheus.AllowAllMetrics {
-			serviceMonitor.Spec.Endpoints[0].MetricRelabelConfigs = []*pov1.RelabelConfig{
-				{
-					Action:       "drop",
-					Regex:        strings.Join(util.DefaultMetricDropList, "|"),
-					SourceLabels: []pov1.LabelName{"__name__"},
-				},
-			}
+		// Remove MetricRelabelConfigs if AllowAllMetrics is set to true
+		if application.Spec.Prometheus != nil && application.Spec.Prometheus.AllowAllMetrics {
+			serviceMonitor.Spec.Endpoints[0].MetricRelabelConfigs = nil
 		}
 
 		return nil

tests/application/service-monitor/application-istio.yaml
@@ -1,12 +1,3 @@
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: sm-istio-ns
-  labels:
-    istio.io/rev: "revision-1"
-
----
-
 apiVersion: skiperator.kartverket.no/v1alpha1
 kind: Application
 metadata:

tests/application/service-monitor/application-simple-assert.yaml
@@ -1,0 +1,92 @@
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: some-simple-monitored-app-1
+  namespace: sm-istio-ns
+spec:
+  selector:
+    matchLabels:
+      app: some-simple-monitored-app-1
+  template:
+    metadata:
+      annotations:
+        argocd.argoproj.io/sync-options: Prune=false
+    spec:
+      containers:
+        - name: some-simple-monitored-app-1
+          image: image
+          imagePullPolicy: Always
+          ports:
+            - containerPort: 8080
+              name: main
+              protocol: TCP
+            # Will actually redirect to istio-proxy
+            - name: istio-metrics
+              containerPort: 15020
+              protocol: TCP
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: some-simple-monitored-app-1
+  namespace: sm-istio-ns
+spec:
+  selector:
+    app: some-simple-monitored-app-1
+  ports:
+    - name: http
+      port: 8080
+      targetPort: 8080
+      protocol: TCP
+      appProtocol: http
+    # Will be used for Prometheus scraping instead of custom port
+    - name: istio-metrics
+      protocol: TCP
+      port: 15020
+      targetPort: 15020
+---
+apiVersion: monitoring.coreos.com/v1
+kind: ServiceMonitor
+metadata:
+  labels:
+    instance: primary
+  name: some-simple-monitored-app-1
+  namespace: sm-istio-ns
+spec:
+  endpoints:
+    - targetPort: istio-metrics
+      path: /stats/prometheus
+      metricRelabelings:
+      - action: drop
+        regex: istio_request_bytes_bucket|istio_response_bytes_bucket|istio_request_duration_milliseconds_bucket
+        sourceLabels:
+        - __name__
+  selector:
+    matchLabels:
+      app: some-simple-monitored-app-1
+  namespaceSelector:
+    matchNames:
+      - sm-istio-ns
+---
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: some-simple-monitored-app-1
+  namespace: sm-istio-ns
+spec:
+  podSelector:
+    matchLabels:
+      app: some-simple-monitored-app-1
+  policyTypes:
+    - Ingress
+  ingress:
+    - from:
+        - namespaceSelector:
+            matchLabels:
+              kubernetes.io/metadata.name: grafana-agent
+          podSelector:
+            matchLabels:
+              app.kubernetes.io/instance: grafana-agent
+              app.kubernetes.io/name: grafana-agent
+      ports:
+        - port: istio-metrics

tests/application/service-monitor/application-simple.yaml
@@ -1,0 +1,16 @@
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: sm-istio-ns
+  labels:
+    istio.io/rev: "revision-1"
+---
+
+apiVersion: skiperator.kartverket.no/v1alpha1
+kind: Application
+metadata:
+  name: some-simple-monitored-app-1
+  namespace: sm-istio-ns
+spec:
+  image: image
+  port: 8080

tests/application/service-monitor/chainsaw-test.yaml
@@ -9,6 +9,11 @@ spec:
   namespace: service-monitor-ns
   steps:
     - try:
+        - apply:
+            file: application-simple.yaml
+        - assert:
+            file: application-simple-assert.yaml
+    - try:
         - create:
             file: application-istio.yaml
         - assert:

tests/skipjob/podmonitor/chainsaw-test.yaml
@@ -18,8 +18,8 @@ spec:
             file: patch-skipjob-with-monitoring.yaml
         - assert:
             file: patch-skipjob-with-monitoring-assert.yaml
-        - delete:
-            ref:
-              apiVersion: skiperator.kartverket.no/v1alpha1
-              kind: SKIPJob
-              name: podmonitor
+    - try:
+        - patch:
+            file: patch-skipjob-remove-monitoring.yaml
+        - error:
+            file: patch-skipjob-remove-monitoring-error.yaml

tests/skipjob/podmonitor/patch-skipjob-remove-monitoring-error.yaml
@@ -1,0 +1,21 @@
+apiVersion: monitoring.coreos.com/v1
+kind: PodMonitor
+metadata:
+  labels:
+    instance: primary
+  name: podmonitor-monitor
+spec:
+  namespaceSelector:
+    matchNames:
+      - podmonitor-ns
+  podMetricsEndpoints:
+    - targetPort: istio-metrics
+      path: "/stats/prometheus"
+      metricRelabelings:
+        - action: drop
+          regex: istio_request_bytes_bucket|istio_response_bytes_bucket|istio_request_duration_milliseconds_bucket
+          sourceLabels:
+            - __name__
+  selector:
+    matchLabels:
+      app: podmonitor

tests/skipjob/podmonitor/patch-skipjob-remove-monitoring.yaml
@@ -1,0 +1,12 @@
+apiVersion: skiperator.kartverket.no/v1alpha1
+kind: SKIPJob
+metadata:
+  name: podmonitor
+spec:
+  container:
+    image: "perl:5.34.0"
+    command:
+      - "perl"
+      - "-Mbignum=bpi"
+      - "-wle"
+      - "print bpi(2000)"