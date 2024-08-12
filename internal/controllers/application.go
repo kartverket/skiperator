b7b87ef2ee6519c89a9aae0619cb2bf2e6565a68
Martin Haram Nygård <martin.nygard@kartverket.no>
2024-08-12 15:00:42 +0200

    rebase

--- internal/controllers/application.go
+++ internal/controllers/application.go
@@ -40,6 +40,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"regexp"
 	ctrl "sigs.k8s.io/controller-runtime"
