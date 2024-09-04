ca300990990408c8c7fd68066d08a26fc18519c4
Bård Ove Hoel <bard.ove.hoel@kartverket.no>
2024-09-04 10:06:58 +0200

    Removed unused functions

--- pkg/util/helperfunctions.go
+++ pkg/util/helperfunctions.go
@@ -9,16 +9,10 @@ import (
 	"github.com/nais/liberator/pkg/namegen"
 	"hash/fnv"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation"
-	"k8s.io/client-go/tools/record"
-	"reflect"
-	"regexp"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strings"
-	"unicode"
 )
 
 // TODO Clean up this file, move functions to more appropriate files
@@ -52,35 +46,6 @@ func GetConfigMap(client client.Client, ctx context.Context, namespacedName type
 	return configMap, err
 }
 
-func GetSecret(client client.Client, ctx context.Context, namespacedName types.NamespacedName) (corev1.Secret, error) {
-	secret := corev1.Secret{}
-
-	err := client.Get(ctx, namespacedName, &secret)
-
-	return secret, err
-}
-
-func GetService(client client.Client, ctx context.Context, namespacedName types.NamespacedName) (corev1.Service, error) {
-	service := corev1.Service{}
-
-	err := client.Get(ctx, namespacedName, &service)
-
-	return service, err
-}
-
-func ErrIsMissingOrNil(recorder record.EventRecorder, err error, message string, object runtime.Object) bool {
-	if errors.IsNotFound(err) {
-		recorder.Eventf(
-			object,
-			corev1.EventTypeWarning, "Missing",
-			message,
-		)
-	} else if err != nil {
-		return false
-	}
-	return true
-}
-
 func ErrDoPanic(err error, message string) {
 	if err != nil {
 		errorMessage := fmt.Sprintf(message, err.Error())
@@ -111,22 +76,8 @@ func GetPodAppAndTeamSelector(applicationName string, teamName string) map[strin
 	}
 }
 
-func HasUpperCaseLetter(word string) bool {
-	for _, letter := range word {
-		if unicode.IsUpper(letter) {
-			return true
-		}
-	}
-
-	return false
-}
-
 func ResourceNameWithKindPostfix(resourceName string, kind string) string {
 	return strings.ToLower(fmt.Sprintf("%v-%v", resourceName, kind))
-}
-
-func GetGatewaySecretName(namespace string, name string) string {
-	return fmt.Sprintf("%s-%s-ingress", namespace, name)
 }
 
 func GetSecretName(prefix string, name string) (string, error) {
@@ -141,21 +92,6 @@ func EnsurePrefix(s string, prefix string) string {
 		return prefix + s
 	}
 	return s
-}
-
-func GetObjectDiff[T any](a T, b T) (diff.Changelog, error) {
-	aKind := reflect.ValueOf(a).Kind()
-	bKind := reflect.ValueOf(b).Kind()
-	if aKind != bKind {
-		return nil, fmt.Errorf("The objects to compare are not the same, found obj1: %v, obj2: %v\n", aKind, bKind)
-	}
-	changelog, err := diff.Diff(a, b)
-
-	if len(changelog) == 0 {
-		return nil, err
-	}
-
-	return changelog, nil
 }
 
 func IsCloudSqlProxyEnabled(gcp *podtypes.GCP) bool {
