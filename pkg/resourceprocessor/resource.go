70f1f9f52c48ce1d783f6cd24ed8019a4472ec48
Martin Haram Nygård <martin.nygard@kartverket.no>
2024-09-27 09:52:40 +0200

    clean up logic a bit

--- pkg/resourceprocessor/resource.go
+++ pkg/resourceprocessor/resource.go
@@ -38,10 +38,10 @@ func preparePatch(new client.Object, old client.Object) {
 		// If the replicas field is not set in the definition, we should set it to 1 if the deployment has 0 replicas or HPA will not work.
 		// If the replicas field is set in the definition, we should use that value so we don't scale down.
 		if definition.Spec.Replicas == nil {
+			definition.Spec.Replicas = deployment.Spec.Replicas
+
 			if *deployment.Spec.Replicas == int32(0) {
 				definition.Spec.Replicas = util.PointTo(int32(1))
-			} else {
-				definition.Spec.Replicas = deployment.Spec.Replicas
 			}
 		}
 		// The command "kubectl rollout restart" puts an annotation on the deployment template in order to track
