caf0a03055d9d5a6a425ecac3d997558f9decd7e
Lars Sørensen <54811127+larsore@users.noreply.github.com>
2025-03-24 12:11:13 +0100

    Bugfix auth policies (#634)
    * Trying to fix up bug introduced when introducing ReqeustAuth
    
    * Update default deny auth policy
    
    * Update test
    
    * Adjust prefix check to not check for wildcard '*'.
    
    * Updating such that allow policies are only created if requestAuth is specified.
    
    * Resolve coderabbit comments
    
    * Add paths from authConfigs to notPaths also when authroizationSettings.allowList is not specified.
    
    * Implement suggestion by coderabbit ai
    
    * Move test from access-policy to jwt-auth

--- pkg/resourcegenerator/istio/authorizationpolicy/common.go
+++ pkg/resourcegenerator/istio/authorizationpolicy/common.go
@@ -1,46 +1,12 @@
 package authorizationpolicy
 
 import (
-	"github.com/kartverket/skiperator/pkg/util"
 	v1 "istio.io/api/security/v1"
-	"istio.io/api/security/v1beta1"
-	typev1beta1 "istio.io/api/type/v1beta1"
-	securityv1 "istio.io/client-go/pkg/apis/security/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 const (
 	DefaultDenyPath = "/actuator*"
 )
-
-func GetAuthPolicy(namespacedName types.NamespacedName, applicationName string, action v1beta1.AuthorizationPolicy_Action, paths []string, notPaths []string) *securityv1.AuthorizationPolicy {
-	return &securityv1.AuthorizationPolicy{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: namespacedName.Namespace,
-			Name:      namespacedName.Name,
-		},
-		Spec: v1.AuthorizationPolicy{
-			Action: action,
-			Rules: []*v1.Rule{
-				{
-					To: []*v1.Rule_To{
-						{
-							Operation: &v1.Operation{
-								Paths:    paths,
-								NotPaths: notPaths,
-							},
-						},
-					},
-					From: GetGeneralFromRule(),
-				},
-			},
-			Selector: &typev1beta1.WorkloadSelector{
-				MatchLabels: util.GetPodAppSelector(applicationName),
-			},
-		},
-	}
-}
 
 func GetGeneralFromRule() []*v1.Rule_From {
 	return []*v1.Rule_From{
--- pkg/resourcegenerator/istio/authorizationpolicy/default_deny/authorization_policy.go
+++ pkg/resourcegenerator/istio/authorizationpolicy/default_deny/authorization_policy.go
@@ -5,8 +5,12 @@ import (
 	skiperatorv1alpha1 "github.com/kartverket/skiperator/api/v1alpha1"
 	"github.com/kartverket/skiperator/pkg/reconciliation"
 	"github.com/kartverket/skiperator/pkg/resourcegenerator/istio/authorizationpolicy"
+	"github.com/kartverket/skiperator/pkg/util"
 	securityv1api "istio.io/api/security/v1"
-	"k8s.io/apimachinery/pkg/types"
+	typev1beta1 "istio.io/api/type/v1beta1"
+	securityv1 "istio.io/client-go/pkg/apis/security/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strings"
 )
 
 func Generate(r reconciliation.Reconciliation) error {
@@ -32,17 +36,47 @@ func Generate(r reconciliation.Reconciliation) error {
 		ctxLog.Debug("No auth config provided. Defaults to deny-all AuthorizationPolicy for application", "application", application.Name)
 	}
 
+	var notPaths []string
+	trimmedPrefix := strings.TrimSuffix(authorizationpolicy.DefaultDenyPath, "*")
+	if application.Spec.AuthorizationSettings != nil {
+		for _, path := range application.Spec.AuthorizationSettings.AllowList {
+			if strings.HasPrefix(path, trimmedPrefix) {
+				notPaths = append(notPaths, path)
+			}
+		}
+	}
+	for _, path := range r.GetAuthConfigs().GetAllPaths() {
+		if strings.HasPrefix(path, trimmedPrefix) {
+			notPaths = append(notPaths, path)
+		}
+	}
+
 	r.AddResource(
-		authorizationpolicy.GetAuthPolicy(
-			types.NamespacedName{
+		&securityv1.AuthorizationPolicy{
+			ObjectMeta: metav1.ObjectMeta{
 				Name:      application.Name + "-default-deny",
 				Namespace: application.Namespace,
 			},
-			application.Name,
-			securityv1api.AuthorizationPolicy_DENY,
-			[]string{defaultDenyPath},
-			[]string{},
-		),
+			Spec: securityv1api.AuthorizationPolicy{
+				Action: securityv1api.AuthorizationPolicy_DENY,
+				Rules: []*securityv1api.Rule{
+					{
+						To: []*securityv1api.Rule_To{
+							{
+								Operation: &securityv1api.Operation{
+									Paths:    []string{defaultDenyPath},
+									NotPaths: notPaths,
+								},
+							},
+						},
+						From: authorizationpolicy.GetGeneralFromRule(),
+					},
+				},
+				Selector: &typev1beta1.WorkloadSelector{
+					MatchLabels: util.GetPodAppSelector(application.Name),
+				},
+			},
+		},
 	)
 
 	ctxLog.Debug("Finished generating default AuthorizationPolicy for application", "application", application.Name)
