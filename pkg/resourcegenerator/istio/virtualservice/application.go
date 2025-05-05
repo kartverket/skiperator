5a1e6743df42fcf8c667ebe6c68874bcb1c0998d
Martin Haram Nygård <martin.nygard@kartverket.no>
2025-05-05 14:41:53 +0200

    SKIP-1676 - add retry option for applications

--- pkg/resourcegenerator/istio/virtualservice/application.go
+++ pkg/resourcegenerator/istio/virtualservice/application.go
@@ -2,13 +2,16 @@ package virtualservice
 
 import (
 	"fmt"
-	"hash/fnv"
-
 	skiperatorv1alpha1 "github.com/kartverket/skiperator/api/v1alpha1"
+	"github.com/kartverket/skiperator/api/v1alpha1/istiotypes"
 	"github.com/kartverket/skiperator/pkg/reconciliation"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"hash/fnv"
 	networkingv1api "istio.io/api/networking/v1"
 	networkingv1 "istio.io/client-go/pkg/apis/networking/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"strconv"
+	"strings"
 )
 
 func init() {
@@ -71,6 +74,7 @@ func generateForApplication(r reconciliation.Reconciliation) error {
 					},
 				},
 			},
+			Retries: generateRetryPolicy(application.Spec.IstioSettings.Retries),
 		})
 		r.AddResource(&virtualService)
 		ctxLog.Debug("Added virtual service to application", "application", application.Name)
@@ -92,4 +96,40 @@ func getGatewaysFromApplication(application *skiperatorv1alpha1.Application) []s
 	}
 
 	return gateways
+}
+
+func generateRetryPolicy(re *istiotypes.Retries) *networkingv1api.HTTPRetry {
+	conditions := "connect-failure,refused-stream,unavailable,cancelled"
+
+	if re == nil {
+		return nil
+	}
+
+	if codes := re.RetryOnHttpResponseCodes; codes != nil && len(*codes) > 0 {
+		var httpRcs []string
+		for _, v := range *codes {
+			if v.StrVal == "" {
+				httpRcs = append(httpRcs, strconv.Itoa(int(v.IntVal)))
+			} else {
+				httpRcs = append(httpRcs, v.StrVal)
+			}
+		}
+		conditions = fmt.Sprintf("%s,%s", conditions, strings.Join(httpRcs, ","))
+	}
+
+	policy := &networkingv1api.HTTPRetry{
+		RetryOn: conditions,
+	}
+
+	// Default to one retry attempt here, avoiding setting default in the type because we do not want issues with argo sync
+	policy.Attempts = int32(1)
+	if re.Attempts != nil {
+		policy.Attempts = *re.Attempts
+	}
+
+	if re.PerTryTimeout != nil {
+		policy.PerTryTimeout = durationpb.New(re.PerTryTimeout.Duration)
+	}
+
+	return policy
 }
