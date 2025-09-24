755ca43bf96f749ab1b8c3ab2c390eb22bf22f95
Even Holthe <even.holthe@kartverket.no>
2025-09-24 11:53:52 +0200

    Annotate KSA by default if GSA in use

--- pkg/resourcegenerator/serviceaccount/service_account.go
+++ pkg/resourcegenerator/serviceaccount/service_account.go
@@ -3,7 +3,6 @@ package serviceaccount
 import (
 	"maps"
 
-	skiperatorv1alpha1 "github.com/kartverket/skiperator/api/v1alpha1"
 	"github.com/kartverket/skiperator/pkg/reconciliation"
 	"github.com/kartverket/skiperator/pkg/resourcegenerator/resourceutils/generator"
 	corev1 "k8s.io/api/core/v1"
@@ -15,13 +14,13 @@ func Generate(r reconciliation.Reconciliation) error {
 	return multiGenerator.Generate(r, "ServiceAccount")
 }
 
-func setCloudSqlAnnotations(serviceAccount *corev1.ServiceAccount, gcp skiperatorv1alpha1.SKIPObject) {
+func setGCPSAAnnotation(serviceAccount *corev1.ServiceAccount, saEmail string) {
 	annotations := serviceAccount.GetAnnotations()
 	if len(annotations) == 0 {
 		annotations = make(map[string]string)
 	}
 	maps.Copy(annotations, map[string]string{
-		"iam.gke.io/gcp-service-account": gcp.GetCommonSpec().GCP.CloudSQLProxy.ServiceAccount,
+		"iam.gke.io/gcp-service-account": saEmail,
 	})
 	serviceAccount.SetAnnotations(annotations)
 }
