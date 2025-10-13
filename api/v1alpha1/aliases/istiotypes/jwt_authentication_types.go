245003eb5d1ae6258115dd87debed1f0889267ab
Andreas Espelund <andreas.espelund@bekk.no>
2025-10-13 09:45:01 +0200

    add all common type aliases for v1alpha1

---
+++ api/v1alpha1/aliases/istiotypes/jwt_authentication_types.go
@@ -0,0 +1,6 @@
+package v1alpha1
+
+import commonistiotypes "github.com/kartverket/skiperator/api/common/istiotypes"
+
+type RequestAuthentication = commonistiotypes.RequestAuthentication
+type ClaimToHeader = commonistiotypes.ClaimToHeader
