f1a60866fc4b220836b8780718da810b2b206c22
Martin Morisbak <martin.morisbak@kartverket.no>
2026-03-04 16:29:32 +0100

    adds test and remove old function

--- pkg/resourcegenerator/volume/volumes.go
+++ pkg/resourcegenerator/volume/volumes.go
@@ -6,93 +6,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
-// Should we split this up? It seems handy to create both from one loop, but the function does two things
-func GetContainerVolumeMountsAndPodVolumes(filesFrom []podtypes.FilesFrom) ([]corev1.Volume, []corev1.VolumeMount) {
-	containerVolumeMounts := []corev1.VolumeMount{
-		{
-			Name:      "tmp",
-			MountPath: "/tmp",
-		},
-	}
-
-	podVolumesMap := map[string]corev1.Volume{
-		"tmp": corev1.Volume{
-			Name: "tmp",
-			VolumeSource: corev1.VolumeSource{
-				EmptyDir: &corev1.EmptyDirVolumeSource{},
-			},
-		},
-	}
-
-	for _, file := range filesFrom {
-		volume := corev1.Volume{}
-		if file.DefaultMode == 0 {
-			file.DefaultMode = 420
-		}
-		if len(file.ConfigMap) > 0 {
-			volume = corev1.Volume{
-				Name: file.ConfigMap,
-				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: file.ConfigMap,
-						},
-						DefaultMode: util.PointTo(int32(file.DefaultMode)),
-					},
-				},
-			}
-		} else if len(file.Secret) > 0 {
-			volume = corev1.Volume{
-				Name: file.Secret,
-				VolumeSource: corev1.VolumeSource{
-					Secret: &corev1.SecretVolumeSource{
-						SecretName:  file.Secret,
-						DefaultMode: util.PointTo(int32(file.DefaultMode)),
-					},
-				},
-			}
-		} else if len(file.EmptyDir) > 0 {
-			volume = corev1.Volume{
-				Name: file.EmptyDir,
-				VolumeSource: corev1.VolumeSource{
-					EmptyDir: &corev1.EmptyDirVolumeSource{},
-				},
-			}
-		} else if len(file.PersistentVolumeClaim) > 0 {
-			volume = corev1.Volume{
-				Name: file.PersistentVolumeClaim,
-				VolumeSource: corev1.VolumeSource{
-					PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-						ClaimName: file.PersistentVolumeClaim,
-					},
-				},
-			}
-		}
-		podVolumesMap[volume.Name] = volume
-
-		if file.SubPath != "" {
-			containerVolumeMounts = append(containerVolumeMounts, corev1.VolumeMount{
-				Name:      volume.Name,
-				MountPath: file.MountPath,
-				SubPath:   file.SubPath,
-			})
-		} else {
-			containerVolumeMounts = append(containerVolumeMounts, corev1.VolumeMount{
-				Name:      volume.Name,
-				MountPath: file.MountPath,
-			})
-		}
-	}
-
-	// remove duplicates from podVolumesMap and convert to slice
-	podVolumes := make([]corev1.Volume, 0, len(podVolumesMap))
-	for _, volume := range podVolumesMap {
-		podVolumes = append(podVolumes, volume)
-	}
-
-	return podVolumes, containerVolumeMounts
-}
-
 func GetContainerVolumeMounts(filesFrom []podtypes.FilesFrom) []corev1.VolumeMount {
 	containerVolumeMounts := []corev1.VolumeMount{
 		{
@@ -131,6 +44,8 @@ func GetContainerVolumeMounts(filesFrom []podtypes.FilesFrom) []corev1.VolumeMou
 }
 
 func GetPodVolumes(filesFrom []podtypes.FilesFrom) []corev1.Volume {
+
+	// Use a map to avoid duplicates
 	podVolumesMap := map[string]corev1.Volume{
 		"tmp": {
 			Name: "tmp",
@@ -139,6 +54,10 @@ func GetPodVolumes(filesFrom []podtypes.FilesFrom) []corev1.Volume {
 			},
 		},
 	}
+
+	// Track insertion order to ensure consistent output for testing
+	insertionOrder := make([]string, 0)
+	insertionOrder = append(insertionOrder, "tmp")
 
 	for _, file := range filesFrom {
 		volume := corev1.Volume{}
@@ -184,13 +103,16 @@ func GetPodVolumes(filesFrom []podtypes.FilesFrom) []corev1.Volume {
 				},
 			}
 		}
-		podVolumesMap[volume.Name] = volume
+
+		if _, exists := podVolumesMap[volume.Name]; !exists {
+			podVolumesMap[volume.Name] = volume
+			insertionOrder = append(insertionOrder, volume.Name)
+		}
 	}
 
-	// remove duplicates from podVolumesMap and convert to slice
-	podVolumes := make([]corev1.Volume, 0, len(podVolumesMap))
-	for _, volume := range podVolumesMap {
-		podVolumes = append(podVolumes, volume)
+	podVolumes := make([]corev1.Volume, 0, len(insertionOrder))
+	for _, key := range insertionOrder {
+		podVolumes = append(podVolumes, podVolumesMap[key])
 	}
 
 	return podVolumes
